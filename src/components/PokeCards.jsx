c6028251a5a8ee1762698ccb106f8e313515b2f4
beagbarreto <33846968+Beagbarreto@users.noreply.github.com>
2021-02-18 23:25:03 -0600

    added images

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { render } from "react-dom";
 
 const Pokemon = (props) => {
   return (
@@ -6,6 +7,10 @@ const Pokemon = (props) => {
       <h1>
         <a href={props.data.videoLink}>{props.data.name}</a>
       </h1>
+      <div>
+        <img src={props.data.sprites.back_default} />
+        <img src={props.data.sprites.back_shiny} />
+      </div>
     </div>
   );
 };
@@ -21,32 +26,11 @@ const PokeCards = (props) => {
 
 export default PokeCards;
 
-// import React, { Component } from "react";
-
-// class PokeCards extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       showShiny: false
-//     };
-//   }
-
-//   render() {
-//     const poke = this.props.poke;
-//     return (
 //       <tr onClick={() => this.setState({ showShiny: !this.state.showShiny })}>
-//         <td>
-//           <h1>
-//             <a href={poke.video}>{poke.name}</a>
-//           </h1>
-//         </td>
+
 //         {/* {this.state.showShiny ? (
 //           <td>{poke.sprites.front_default}</td>
 //         ) : (
 //           <td>{poke.sprites.back_shiny} </td>
 //         )} */}
 //       </tr>
-//     );
-//   }
-// }
-// export default PokeCards;
